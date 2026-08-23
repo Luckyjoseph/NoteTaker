4c28bb98ba8a01c98285070a8b1e085c5b81be65
luckyjoseph <luckyjoseph1996@gmail.com>
2026-08-23 16:09:09 +0100

    Added all project files and readme file

---
+++ app/build.gradle.kts
@@ -0,0 +1,72 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("kotlin-parcelize")
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.navigation.safeargs)
+}
+
+android {
+    namespace = "com.example.notestaker"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.notestaker"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        dataBinding = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.material)
+
+    // Room
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+
+    // Coroutines
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
+    // Navigation
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+
+    // Lifecycle
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.junit)
+}
---
+++ app/src/main/java/com/example/notestaker/repository/NoteRepository.kt
@@ -0,0 +1,15 @@
+package com.example.notestaker.repository
+
+import com.example.notestaker.database.NoteData
+import com.example.notestaker.model.Note
+
+class NoteRepository(private val db: NoteData) {
+
+    suspend fun insertNote(note: Note) = db.getNoteDao().insertNote(note)
+    suspend fun updateNote(note: Note) = db.getNoteDao().updateNote(note)
+    suspend fun deleteNote(note: Note) =db.getNoteDao().deleteNote(note)
+
+
+    fun getAllNotes() = db.getNoteDao().getAllNotes()
+    fun searchNote(query: String?) =db.getNoteDao().searchNote(query)
+}
